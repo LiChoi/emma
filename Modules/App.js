bae2ea3d350443592d9d0343b445a29c9072edfa
LC <53445655+LiChoi@users.noreply.github.com>
2019-12-01 13:52:35 -0500

    removed unneeded import statement

--- Modules/App.js
+++ Modules/App.js
@@ -340,7 +340,7 @@ class App extends Component {
         {renderMedlist(this.state, this.updateState)}
         {renderTakePicture(this.state, this.updateState)}
         {renderEmmaAsks(this.state, this.updateState)}
-        {renderAbout(this.state, this.updateState)}
+        {renderAbout(this.state)}
         {this.state.screen !== 'home' ? <BarButton color='rgba(0, 155, 95, 1)' title="Home" onPress={()=>{this.updateState('by path and value', {path: 'screen', value: 'home'})}} /> : null } 
         {this.state.screen == 'home' ? <BarButton title='Update' onPress={()=>{ updateCompendium({updateRealm: this.updateRealm, state: this.state, updateState: this.updateState}); }} /> : null}
         {this.state.screen == 'home' ? <BarButton title='About Emma' onPress={()=>{ this.updateState('by path and value', {path: 'screen', value: 'about'}) }} /> : null}
