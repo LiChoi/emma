bfe64537e9848bcfde2764b83833cb19cbe5496a
LC <53445655+LiChoi@users.noreply.github.com>
2019-12-15 12:09:03 -0500

    Release version need to comment out debug signing in build.gradle

--- Modules/About.js
+++ Modules/About.js
@@ -19,23 +19,21 @@ export const renderAbout = (state) => {
                 <Text></Text>
                 <Text>Keep a list of your spouse's medical conditions.</Text>
                 <Text></Text>
-                <Text>Keep a list of your parent's medications, including details such as dose, directions, prescriber, and even a picture of what the pill looks like. Neat!</Text>
+                <Text>Keep a list of your parent's medications, including details such as dose, directions, and even a picture of what the pill looks like. Neat!</Text>
                 <Text></Text>
                 <Text style={styles.label}>Emma Asks</Text>
                 <Text></Text>
                 <Text>Emma isn't merely a humble keeper-of-lists. She's also quite smart!</Text>
                 <Text></Text>
-                <Text>She can analyze the information you provide and suggest questions for you to ask of your medical provider - just to make extra sure that your medicines are safe and effective.</Text>
+                <Text>She can analyze the information you provide and suggest questions for you to ask of your medical provider - just to make extra sure that your medicines are safe and effective*.</Text>
                 <Text></Text>
-                <Text>Please don't think that Emma is second-guessing your highly educated and experienced medical provider. Her medical knowledge is far from complete, and her analytical capabilities require further refinement.</Text>
+                <Text>If you need to send this information to a medical provider, Emma can prepare a draft of the email body so you don't have to type it all out. Convenient!</Text>
                 <Text></Text>
-                <Text>Despite her limitations, she feels she can still help by identifying intelligent questions for you to ask. In most cases, your doctor or pharmacist should already aware of any issues that Emma might raise, but there's no harm in asking anyway.</Text>
-                <Text></Text>
-                <Text>Whenever Emma suggests questions, you can ask her to email your doctor or pharmacist directly. Conventient!</Text>
+                <Text>*Emma is not a substitute for a licensed medical practicioner. Her medical knowledge is far from complete, and her analytical capabilities require further refinement. Ideally, your medical provider(s) should already be aware of any issues that Emma might raise, but there's no harm in asking anyway. Do not make any decision to alter your medication therapy based on Emma's questions without first consulting with a medical practitioner.</Text>
                 <Text></Text>
                 <Text style={styles.label}>Privacy Policy</Text>
                 <Text></Text>
-                <Text>Emma stores all the information you provide to her on your phone. Therefore, this information is only as secure as your phone. It is your responsibility to keep your phone secure.</Text>
+                <Text>Emma stores all the information you provide to her on your phone. Therefore, this information is only as secure as your phone. It is your responsibility to keep your phone safe.</Text>
                 <Text></Text>
                 <Text>Emma does not transmit this information anywhere else except when you ask her to open up your phone's gmail account to pre-fill the email body. Even then, you must input the email address and hit 'send' before any information is sent.</Text>
                 <Text></Text>
